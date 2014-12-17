365ed28cc700d7a0ba62595a0c6e75a8cc76291c
Egon Zemmer <egon.zemmer@phlegx.com>
2014-12-17 19:58:51 +0100

    Bump to version 0.1.1.

--- init.rb
+++ init.rb
@@ -4,9 +4,9 @@ Redmine::Plugin.register :redmine_gitlab_hook do
   name 'Redmine GitLab Hook plugin'
   author 'Phlegx Systems'
   description 'This plugin allows your Redmine installation to receive GitLab post-receive notifications'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/phlegx/redmine_gitlab_hook'
   author_url 'https://github.com/phlegx'
-  
+
   settings default: { all_branches: 'yes', prune: 'yes' }, partial: 'settings/gitlab_settings'
 end

f56cd2a363fdc91d2095bfd6b396245efff35b54
Glenn Huang <dkstu.tw@gmail.com>
2019-06-10 10:42:48 +0800

    Issue #45 fix :text => :plain

--- app/controllers/gitlab_hook_controller.rb
+++ app/controllers/gitlab_hook_controller.rb
@@ -19,9 +19,9 @@ class GitlabHookController < ActionController::Base
         if git_success
           # Fetch the new changesets into Redmine
           repository.fetch_changesets
-          render(:text => 'OK', :status => :ok)
+          render(:plain => 'OK', :status => :ok)
         else
-          render(:text => "Git command failed on repository: #{repository.identifier}!", :status => :not_acceptable)
+          render(:plain => "Git command failed on repository: #{repository.identifier}!", :status => :not_acceptable)
         end
       end
     else

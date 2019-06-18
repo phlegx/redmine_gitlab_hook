4f98f40eb678198d6e2e0614c84d09aee54c9e2f
Phlegx Systems OG <egon.zemmer@phlegx.com>
2019-06-18 16:46:53 +0200

    Bump to version 4.0.0 and minimal Redmine version 3.0.0.

--- init.rb
+++ init.rb
@@ -4,10 +4,15 @@ Redmine::Plugin.register :redmine_gitlab_hook do
   name 'Redmine GitLab Hook plugin'
   author 'Phlegx Systems'
   description 'This plugin allows your Redmine installation to receive GitLab post-receive notifications'
-  version '0.2.2'
+  version '4.0.0'
   url 'https://github.com/phlegx/redmine_gitlab_hook'
   author_url 'https://github.com/phlegx'
-  requires_redmine :version_or_higher => '2.3.0'
+  requires_redmine :version_or_higher => '3.0.0'
 
-  settings :default => { :all_branches => 'yes', :prune => 'yes', :auto_create => 'yes', :fetch_updates => 'yes' }, :partial => 'settings/gitlab_settings'
+  settings :default => { 
+    :all_branches => 'yes', 
+    :prune => 'yes', 
+    :auto_create => 'yes', 
+    :fetch_updates => 'yes' 
+  }, :partial => 'settings/gitlab_settings'
 end
